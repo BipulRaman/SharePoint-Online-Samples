c1884f1d0f97d01c51e23917d2b4408629b5e152
Bipul Raman <8312486+BipulRaman@users.noreply.github.com>
2021-07-09 13:21:51 +0530

    Auth Managers Updated.

--- CSharp/SPO.Samples/SPO.ModernAuthentication/Helpers/ServicePrincipalAuthenticationManager.cs
+++ CSharp/SPO.Samples/SPO.ModernAuthentication/Helpers/ServicePrincipalAuthenticationManager.cs
@@ -1,66 +1,78 @@
-﻿using Microsoft.Identity.Client;
-using Microsoft.SharePoint.Client;
-using System;
-using System.Collections.Concurrent;
-using System.Net.Http;
-using System.Security;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web;
+﻿// <copyright file="ServicePrincipalAuthenticationManager.cs" company="Bipul Raman">
+// Copyright (c) Bipul Raman. All rights reserved.
+// </copyright>
 
 namespace SPO.ModernAuthentication.Helpers
 {
+    using System;
+    using System.Collections.Concurrent;
+    using System.Net.Http;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Identity.Client;
+    using Microsoft.SharePoint.Client;
+
+    /// <summary>
+    /// ServicePrincipalAuthenticationManager Class.
+    /// </summary>
     public class ServicePrincipalAuthenticationManager : IDisposable
     {
-        private static readonly HttpClient httpClient = new HttpClient();
-        private const string tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/token";
+        private static readonly HttpClient HttpClient = new HttpClient();
 
         // Token cache handling
-        private static readonly SemaphoreSlim semaphoreSlimTokens = new SemaphoreSlim(1);
-        private AutoResetEvent tokenResetEvent = null;
+        private static readonly SemaphoreSlim SemaphoreSlimTokens = new SemaphoreSlim(1);
         private readonly ConcurrentDictionary<string, string> tokenCache = new ConcurrentDictionary<string, string>();
+        private AutoResetEvent tokenResetEvent = null;
         private bool disposedValue;
 
-        internal class TokenWaitInfo
-        {
-            public RegisteredWaitHandle Handle = null;
-        }
-
+        /// <summary>
+        /// Method to Get SPO Client Context.
+        /// </summary>
+        /// <param name="web">SPO Web Url.</param>
+        /// <param name="tenantId">SPO Tenant ID.</param>
+        /// <param name="clientId">AAD App Client ID.</param>
+        /// <param name="clientCertificate">AAD Client Certificate.</param>
+        /// <returns>SPO Client Context.</returns>
         public ClientContext GetContext(Uri web, string tenantId, string clientId, X509Certificate2 clientCertificate)
         {
             var context = new ClientContext(web);
 
             context.ExecutingWebRequest += (sender, e) =>
             {
-                string accessToken = EnsureAccessTokenAsync(new Uri($"{web.Scheme}://{web.DnsSafeHost}"), tenantId, clientId, clientCertificate).GetAwaiter().GetResult();
+                string accessToken = this.EnsureAccessTokenAsync(new Uri($"{web.Scheme}://{web.DnsSafeHost}"), tenantId, clientId, clientCertificate).GetAwaiter().GetResult();
                 e.WebRequestExecutor.RequestHeaders["Authorization"] = "Bearer " + accessToken;
             };
 
             return context;
         }
 
-
+        /// <summary>
+        /// Method to Ensure Access Token Async.
+        /// </summary>
+        /// <param name="resourceUri">Resource Uri.</param>
+        /// <param name="tenantId">Tenant Id.</param>
+        /// <param name="clientId">Client Id.</param>
+        /// <param name="clientCertificate">Client Certificate.</param>
+        /// <returns>Access Token.</returns>
         public async Task<string> EnsureAccessTokenAsync(Uri resourceUri, string tenantId, string clientId, X509Certificate2 clientCertificate)
         {
-            string accessTokenFromCache = TokenFromCache(resourceUri, tokenCache);
+            string accessTokenFromCache = TokenFromCache(resourceUri, this.tokenCache);
             if (accessTokenFromCache == null)
             {
-                await semaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
+                await SemaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
                 try
                 {
                     // No async methods are allowed in a lock section
-                    string accessToken = await AcquireTokenAsync(resourceUri, tenantId, clientId, clientCertificate).ConfigureAwait(false);
+                    string accessToken = await this.AcquireTokenAsync(resourceUri, tenantId, clientId, clientCertificate).ConfigureAwait(false);
                     Console.WriteLine($"Successfully requested new access token resource {resourceUri.DnsSafeHost} for user {clientId}");
-                    AddTokenToCache(resourceUri, tokenCache, accessToken);
+                    AddTokenToCache(resourceUri, this.tokenCache, accessToken);
 
                     // Register a thread to invalidate the access token once's it's expired
-                    tokenResetEvent = new AutoResetEvent(false);
+                    this.tokenResetEvent = new AutoResetEvent(false);
                     TokenWaitInfo wi = new TokenWaitInfo();
                     wi.Handle = ThreadPool.RegisterWaitForSingleObject(
-                        tokenResetEvent,
+                        this.tokenResetEvent,
                         async (state, timedOut) =>
                         {
                             if (!timedOut)
@@ -76,32 +88,30 @@ namespace SPO.ModernAuthentication.Helpers
                                 try
                                 {
                                     // Take a lock to ensure no other threads are updating the SharePoint Access token at this time
-                                    await semaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
-                                    RemoveTokenFromCache(resourceUri, tokenCache);
+                                    await SemaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
+                                    RemoveTokenFromCache(resourceUri, this.tokenCache);
                                     Console.WriteLine($"Cached token for resource {resourceUri.DnsSafeHost} and user {clientId} expired");
                                 }
                                 catch (Exception ex)
                                 {
                                     Console.WriteLine($"Something went wrong during cache token invalidation: {ex.Message}");
-                                    RemoveTokenFromCache(resourceUri, tokenCache);
+                                    RemoveTokenFromCache(resourceUri, this.tokenCache);
                                 }
                                 finally
                                 {
-                                    semaphoreSlimTokens.Release();
+                                    SemaphoreSlimTokens.Release();
                                 }
                             }
                         },
                         wi,
                         (uint)CalculateThreadSleep(accessToken).TotalMilliseconds,
-                        true
-                    );
+                        true);
 
                     return accessToken;
-
                 }
                 finally
                 {
-                    semaphoreSlimTokens.Release();
+                    SemaphoreSlimTokens.Release();
                 }
             }
             else
@@ -111,16 +121,35 @@ namespace SPO.ModernAuthentication.Helpers
             }
         }
 
-        private async Task<string> AcquireTokenAsync(Uri resourceUri, string tenantId, string clientId, X509Certificate2 clientCertificate)
+        /// <summary>
+        /// Dispose Method.
+        /// </summary>
+        public void Dispose()
         {
-            string[] scopes = new string[] { $"{resourceUri.Scheme}://{resourceUri.DnsSafeHost}/.default" };
-            var app = ConfidentialClientApplicationBuilder.Create(clientId)
-            .WithTenantId(tenantId)
-            .WithCertificate(clientCertificate)
-            .Build();
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
 
-            var result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
-            return result.AccessToken;
+        /// <summary>
+        /// Dispose Method.
+        /// </summary>
+        /// <param name="disposing">Disposing Value.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                if (disposing)
+                {
+                    if (this.tokenResetEvent != null)
+                    {
+                        this.tokenResetEvent.Set();
+                        this.tokenResetEvent.Dispose();
+                    }
+                }
+
+                this.disposedValue = true;
+            }
         }
 
         private static string TokenFromCache(Uri web, ConcurrentDictionary<string, string> tokenCache)
@@ -166,28 +195,27 @@ namespace SPO.ModernAuthentication.Helpers
             return lease;
         }
 
-        protected virtual void Dispose(bool disposing)
+        private async Task<string> AcquireTokenAsync(Uri resourceUri, string tenantId, string clientId, X509Certificate2 clientCertificate)
         {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    if (tokenResetEvent != null)
-                    {
-                        tokenResetEvent.Set();
-                        tokenResetEvent.Dispose();
-                    }
-                }
+            string[] scopes = new string[] { $"{resourceUri.Scheme}://{resourceUri.DnsSafeHost}/.default" };
+            var app = ConfidentialClientApplicationBuilder.Create(clientId)
+            .WithTenantId(tenantId)
+            .WithCertificate(clientCertificate)
+            .Build();
 
-                disposedValue = true;
-            }
+            var result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
+            return result.AccessToken;
         }
 
-        public void Dispose()
+        /// <summary>
+        /// TokenWaitInfo Internal Class.
+        /// </summary>
+        internal class TokenWaitInfo
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
+            /// <summary>
+            /// RegisteredWaitHandle.
+            /// </summary>
+            public RegisteredWaitHandle Handle = null;
         }
     }
 }
--- CSharp/SPO.Samples/SPO.ModernAuthentication/Helpers/UserAuthenticationManager.cs
+++ CSharp/SPO.Samples/SPO.ModernAuthentication/Helpers/UserAuthenticationManager.cs
@@ -1,64 +1,83 @@
-﻿using Microsoft.SharePoint.Client;
-using System;
-using System.Collections.Concurrent;
-using System.Net.Http;
-using System.Security;
-using System.Text;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web;
+﻿// <copyright file="UserAuthenticationManager.cs" company="Bipul Raman">
+// Copyright (c) Bipul Raman. All rights reserved.
+// </copyright>
 
 namespace SPO.ModernAuthentication.Helpers
 {
+    using System;
+    using System.Collections.Concurrent;
+    using System.Net.Http;
+    using System.Security;
+    using System.Text;
+    using System.Text.Json;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Microsoft.SharePoint.Client;
+
+    /// <summary>
+    /// UserAuthenticationManager Class.
+    /// </summary>
     public class UserAuthenticationManager : IDisposable
     {
-        private static readonly HttpClient httpClient = new HttpClient();
-        private const string tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/token";
+
+        private const string TokenEndpoint = "https://login.microsoftonline.com/common/oauth2/token";
+        private static readonly HttpClient HttpClient = new HttpClient();
 
         // Token cache handling
-        private static readonly SemaphoreSlim semaphoreSlimTokens = new SemaphoreSlim(1);
-        private AutoResetEvent tokenResetEvent = null;
+        private static readonly SemaphoreSlim SemaphoreSlimTokens = new SemaphoreSlim(1);
         private readonly ConcurrentDictionary<string, string> tokenCache = new ConcurrentDictionary<string, string>();
+        private AutoResetEvent tokenResetEvent = null;
+
         private bool disposedValue;
 
-        internal class TokenWaitInfo
-        {
-            public RegisteredWaitHandle Handle = null;
-        }
-
+        /// <summary>
+        /// Method to get SPO Context.
+        /// </summary>
+        /// <param name="web">SPO Web Url.</param>
+        /// <param name="userPrincipalName">UserName.</param>
+        /// <param name="userPassword">Password.</param>
+        /// <param name="clientId">AAD App Client Id.</param>
+        /// <returns>SPO Client Context.</returns>
         public ClientContext GetContext(Uri web, string userPrincipalName, SecureString userPassword, string clientId)
         {
             var context = new ClientContext(web);
 
             context.ExecutingWebRequest += (sender, e) =>
             {
-                string accessToken = EnsureAccessTokenAsync(new Uri($"{web.Scheme}://{web.DnsSafeHost}"), userPrincipalName, new System.Net.NetworkCredential(string.Empty, userPassword).Password, clientId).GetAwaiter().GetResult();
+                string accessToken = this.EnsureAccessTokenAsync(new Uri($"{web.Scheme}://{web.DnsSafeHost}"), userPrincipalName, new System.Net.NetworkCredential(string.Empty, userPassword).Password, clientId).GetAwaiter().GetResult();
                 e.WebRequestExecutor.RequestHeaders["Authorization"] = "Bearer " + accessToken;
             };
 
             return context;
         }
 
-
+        /// <summary>
+        /// Method to Ensure Access Token Async.
+        /// </summary>
+        /// <param name="resourceUri">Resource Uri.</param>
+        /// <param name="userPrincipalName">UPN.</param>
+        /// <param name="userPassword">UserName.</param>
+        /// <param name="clientId">AAD App Client Id.</param>
+        /// <returns>Access Token.</returns>
         public async Task<string> EnsureAccessTokenAsync(Uri resourceUri, string userPrincipalName, string userPassword, string clientId)
         {
-            string accessTokenFromCache = TokenFromCache(resourceUri, tokenCache);
+            string accessTokenFromCache = TokenFromCache(resourceUri, this.tokenCache);
             if (accessTokenFromCache == null)
             {
-                await semaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
+                await SemaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
                 try
                 {
                     // No async methods are allowed in a lock section
-                    string accessToken = await AcquireTokenAsync(resourceUri, userPrincipalName, userPassword, clientId).ConfigureAwait(false);
+                    string accessToken = await this.AcquireTokenAsync(resourceUri, userPrincipalName, userPassword, clientId).ConfigureAwait(false);
                     Console.WriteLine($"Successfully requested new access token resource {resourceUri.DnsSafeHost} for user {userPrincipalName}");
-                    AddTokenToCache(resourceUri, tokenCache, accessToken);
+                    AddTokenToCache(resourceUri, this.tokenCache, accessToken);
 
                     // Register a thread to invalidate the access token once's it's expired
-                    tokenResetEvent = new AutoResetEvent(false);
+                    this.tokenResetEvent = new AutoResetEvent(false);
                     TokenWaitInfo wi = new TokenWaitInfo();
                     wi.Handle = ThreadPool.RegisterWaitForSingleObject(
-                        tokenResetEvent,
+                        this.tokenResetEvent,
                         async (state, timedOut) =>
                         {
                             if (!timedOut)
@@ -74,32 +93,30 @@ namespace SPO.ModernAuthentication.Helpers
                                 try
                                 {
                                     // Take a lock to ensure no other threads are updating the SharePoint Access token at this time
-                                    await semaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
-                                    RemoveTokenFromCache(resourceUri, tokenCache);
+                                    await SemaphoreSlimTokens.WaitAsync().ConfigureAwait(false);
+                                    RemoveTokenFromCache(resourceUri, this.tokenCache);
                                     Console.WriteLine($"Cached token for resource {resourceUri.DnsSafeHost} and user {userPrincipalName} expired");
                                 }
                                 catch (Exception ex)
                                 {
                                     Console.WriteLine($"Something went wrong during cache token invalidation: {ex.Message}");
-                                    RemoveTokenFromCache(resourceUri, tokenCache);
+                                    RemoveTokenFromCache(resourceUri, this.tokenCache);
                                 }
                                 finally
                                 {
-                                    semaphoreSlimTokens.Release();
+                                    SemaphoreSlimTokens.Release();
                                 }
                             }
                         },
                         wi,
                         (uint)CalculateThreadSleep(accessToken).TotalMilliseconds,
-                        true
-                    );
+                        true);
 
                     return accessToken;
-
                 }
                 finally
                 {
-                    semaphoreSlimTokens.Release();
+                    SemaphoreSlimTokens.Release();
                 }
             }
             else
@@ -109,22 +126,34 @@ namespace SPO.ModernAuthentication.Helpers
             }
         }
 
-        private async Task<string> AcquireTokenAsync(Uri resourceUri, string username, string password, string clientId)
+        /// <summary>
+        /// Dispose Method.
+        /// </summary>
+        public void Dispose()
         {
-            string resource = $"{resourceUri.Scheme}://{resourceUri.DnsSafeHost}";
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
 
-            var body = $"resource={resource}&client_id={clientId}&grant_type=password&username={HttpUtility.UrlEncode(username)}&password={HttpUtility.UrlEncode(password)}";
-            using (var stringContent = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded"))
+        /// <summary>
+        /// Dispose Method.
+        /// </summary>
+        /// <param name="disposing">Disposing.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
             {
-
-                var result = await httpClient.PostAsync(tokenEndpoint, stringContent).ContinueWith((response) =>
+                if (disposing)
                 {
-                    return response.Result.Content.ReadAsStringAsync().Result;
-                }).ConfigureAwait(false);
+                    if (this.tokenResetEvent != null)
+                    {
+                        this.tokenResetEvent.Set();
+                        this.tokenResetEvent.Dispose();
+                    }
+                }
 
-                var tokenResult = JsonSerializer.Deserialize<JsonElement>(result);
-                var token = tokenResult.GetProperty("access_token").GetString();
-                return token;
+                this.disposedValue = true;
             }
         }
 
@@ -171,28 +200,30 @@ namespace SPO.ModernAuthentication.Helpers
             return lease;
         }
 
-        protected virtual void Dispose(bool disposing)
+        private async Task<string> AcquireTokenAsync(Uri resourceUri, string username, string password, string clientId)
         {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    if (tokenResetEvent != null)
-                    {
-                        tokenResetEvent.Set();
-                        tokenResetEvent.Dispose();
-                    }
-                }
+            string resource = $"{resourceUri.Scheme}://{resourceUri.DnsSafeHost}";
 
-                disposedValue = true;
+            var body = $"resource={resource}&client_id={clientId}&grant_type=password&username={HttpUtility.UrlEncode(username)}&password={HttpUtility.UrlEncode(password)}";
+            using (var stringContent = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded"))
+            {
+                var result = await HttpClient.PostAsync(TokenEndpoint, stringContent).ContinueWith((response) =>
+                {
+                    return response.Result.Content.ReadAsStringAsync().Result;
+                }).ConfigureAwait(false);
+
+                var tokenResult = JsonSerializer.Deserialize<JsonElement>(result);
+                var token = tokenResult.GetProperty("access_token").GetString();
+                return token;
             }
         }
 
-        public void Dispose()
+        /// <summary>
+        /// TokenWaitInfo Class.
+        /// </summary>
+        internal class TokenWaitInfo
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
+            public RegisteredWaitHandle Handle = null;
         }
     }
 }
